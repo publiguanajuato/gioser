0a071df5f5bbb7d7b636b196ed24bc50820e3d77
publiguanajuato <87594963+publiguanajuato@users.noreply.github.com>
2021-07-23 12:51:28 -0500

    Add files via upload

---
+++ guardarticulo.php
@@ -0,0 +1,7 @@
+﻿<?php
+$fi= fopen("articulo2.txt", "a")or die ("problemas al crear archivo")
+fwrite($fi, $_POST['NOMBRE']);
+fwrite($fi, "\n");
+fwrite($fi, $_POST['ARTICULO']);
+fwrite($fi, "\n");
+?>
